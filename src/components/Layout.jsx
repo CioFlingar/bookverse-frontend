1d7d2715cd4ced0a3074ac07e49c43ddeda6ccbd
Mahi <coder.sumayamahira@gmail.com>
2026-05-10 12:23:14 +0600

    update layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,25 +1,30 @@
 // src/components/Layout.jsx
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen bg-verse-cream font-sans text-verse-dark">
+    <div className="min-h-screen bg-gray-50 font-sans text-slate-800">
       <Navbar />
 
-      <main className="max-w-7xl mx-auto py-10 px-8">
-        {children}
-      </main>
+      <main className="max-w-7xl mx-auto py-10 px-8">{children}</main>
 
-      <footer className="mt-20 border-t border-gray-200 py-12 px-8">
+      {/* Footer & links */}
+      <footer className="mt-10 border-t border-gray-200 py-10 px-8">
         <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center gap-6">
-          <p className="text-xs text-gray-400 uppercase tracking-widest">
-            2024 BOOKVERSE. THE ART OF READING [cite: 35, 84]
-          </p>
+          <div flex flex-col >
+            <div class="mb-4 md:mb-0 font-bold text-slate-800 italic">
+              BookVerse
+            </div>
+            <p className="text-xs text-gray-500 uppercase tracking-widest">
+              © 2024 BOOKVERSE. THE ART OF READING.
+            </p>
+          </div>
+
           <div className="flex gap-6 text-xs text-gray-500 uppercase tracking-tighter font-bold">
-            <a href="#">About our Press</a>
-            <a href="#">Shipping & Returns</a>
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms of Service</a>
+            <a href="#" className="hover:text-slate-800 underline">About our Press</a>
+            <a href="#" className="hover:text-slate-800 underline">Shipping & Returns</a>
+            <a href="#" className="hover:text-slate-800 underline">Privacy Policy</a>
+            <a href="#" className="hover:text-slate-800 underline">Terms of Service</a>
           </div>
         </div>
       </footer>
