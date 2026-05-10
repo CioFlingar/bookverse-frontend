b3e56b284d94cb34fe082cf03b9d258e65a168ad
Mahi <coder.sumayamahira@gmail.com>
2026-05-10 15:13:39 +0600

    update quote section design

--- src/features/home/Quote.jsx
+++ src/features/home/Quote.jsx
@@ -1,15 +1,18 @@
 // src/features/home/Quote.jsx
 export default function Quote() {
   return (
-    <div className="py-10 border-t border-gray-200 text-left">
-        <blockquote className="max-w-2xl mx-auto">
+    <div className="py-10 border-t border-gray-200 flex justify-center">
+        <div className="border-red-400 border-l-2 py-8 pl-6 md:pl-10">
+            <blockquote className="max-w-2xl">
             <p className="font-serif italic text-xs md:text-xl text-slate-700 leading-snug mb-4">
           "A room without books is like a body without a soul. BookVerse has curated a collection that speaks to both the intellect and the spirit."
             </p>
             <footer className="text-[10px] uppercase tracking-[0.3em] text-gray-400 font-bold">
-          - MARCUS AURELIUS, COLLECTOR
+          — MARCUS AURELIUS, COLLECTOR
             </footer>
         </blockquote>
+        </div>
+
     </div>
 
   );
