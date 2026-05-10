80693e644b6519af7398bef263915092ad0f8926
Mahi <coder.sumayamahira@gmail.com>
2026-05-10 21:20:49 +0600

    update quote section design

--- src/features/home/Quote.jsx
+++ src/features/home/Quote.jsx
@@ -2,7 +2,7 @@
 export default function Quote() {
   return (
     <div className="py-10 border-t border-gray-200 flex justify-center">
-        <div className="border-red-400 border-l-2 py-8 pl-6 md:pl-10">
+        <div className="border-red-400 border-l py-8 pl-6 md:pl-10">
             <blockquote className="max-w-2xl">
             <p className="font-serif italic text-xs md:text-xl text-slate-700 leading-snug mb-4">
           "A room without books is like a body without a soul. BookVerse has curated a collection that speaks to both the intellect and the spirit."
